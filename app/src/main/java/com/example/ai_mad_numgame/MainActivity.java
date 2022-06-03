a4651934ba0ba0b8d46d1c79549aaf77026ecf24
Pal-Karan <201b353@juetguna.in>
2022-06-04 02:22:34 +0530

    Added the newMatch()

--- app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
+++ app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
@@ -72,12 +72,54 @@ public class MainActivity extends AppCompatActivity {
     public void newMatch() {  //A game is composed of three matches
 
         int operand1 = random.nextInt(10);
-        int operand2=0;
+        int operand2=random.nextInt(6);
         //check is operand2 is not zero; otherwise in case of division-divide by zero error will come
         String operator = operators[random.nextInt(4)];
         textView2.setText(operand1 + operator + operand2);
-
-      // Your code here, to diplay correct and incorrect options on the buttons
+        int correctans=0;
+        int min=0,max=3;
+        correctButton=(int) Math.floor(Math.random() * (max - min+1) + min);
+        if(operator=="+")
+            correctans=operand1+operand2;
+        else if(operator=="*")
+            correctans=operand1*operand2;
+        else if(operator=="-")
+            correctans=operand1-operand2;
+        else if(operator=="/")
+            correctans=operand1/operand2;
+//        button1.setText(Integer.toString(correctans));
+//        button2.setText(Integer.toString(correctans+1));
+//        button3.setText(Integer.toString(correctans-3));
+//        button4.setText(Integer.toString(correctans+2));
+        if(correctButton==0)
+        {
+            button1.setText(Integer.toString(correctans));
+            button2.setText(Integer.toString(correctans+1));
+            button3.setText(Integer.toString(correctans-3));
+            button4.setText(Integer.toString(correctans+2));
+        }
+        else if(correctButton==1)
+        {
+            button1.setText(Integer.toString(correctans-1));
+            button2.setText(Integer.toString(correctans));
+            button3.setText(Integer.toString(correctans-3));
+            button4.setText(Integer.toString(correctans+2));
+        }
+        else if(correctButton==2)
+        {
+            button1.setText(Integer.toString(correctans-1));
+            button2.setText(Integer.toString(correctans-8));
+            button3.setText(Integer.toString(correctans));
+            button4.setText(Integer.toString(correctans+2));
+        }
+        else if(correctButton==3)
+        {
+            button1.setText(Integer.toString(correctans-1));
+            button2.setText(Integer.toString(correctans+7));
+            button3.setText(Integer.toString(correctans-3));
+            button4.setText(Integer.toString(correctans));
+        }
+        // Your code here, to diplay correct and incorrect options on the buttons
 
         if(matchCounter==3){    // if three matches are completed updatee the perfomrance in sharedpreferences
 
@@ -91,6 +133,7 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+
 
     public int sumOfScore(){
         //Computing the sum of score array, which has the 1 or in each index,depending on correct or incorrect answers
